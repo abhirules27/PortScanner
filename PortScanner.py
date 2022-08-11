3ac5254961b4af9ad85a4c6c1652dc83080cb564
Abhishek Mishra <abhirules27@gmail.com>
2022-08-11 13:18:05 +0530

    Debugged Fully functional code now

--- PortScanner.py
+++ PortScanner.py
@@ -1,1 +1,31 @@
 # PortScanner with Threads
+import socket
+import threading
+import queue
+
+IP = '192.168.0.181'
+q = queue.Queue()
+
+# Storing port Nos in Queue
+for port in range (55575, 55578):
+    q.put(port)
+
+
+def scan():
+    while not q.empty():
+        port = q.get()
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                s.connect((IP, port))
+                print(f'Port {port} is open!')
+            except:
+                pass
+        q.task_done()
+
+# Create No of threads we want to use
+for port in range(30):
+    t = threading.Thread(target=scan, daemon=True)
+    t.start()
+
+q.join()
+print('Finished')
